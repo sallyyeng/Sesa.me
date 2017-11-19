0f370c66d4dcfaf097e1fe556d4c92de4556773e
Rebecca Townsend <rebecca.townsend722@gmail.com>
2017-11-19 13:35:29 -0800

    Set up express router

--- server/routes.js
+++ server/routes.js
@@ -1,1 +1,14 @@
 //This file sets up express router
+var controller = require('./controllers/index.js');
+var router = require('express').Router();
+
+router.get('/login', controller.login.get);
+router.post('/login', controller.login.post);
+
+router.get('/signup', controller.signup.get);
+router.post('/signup', controller.signup.post);
+
+router.get('/submissions', controller.submissions.get);
+router.post('/submissions', controller.submissions.post);
+
+module.exports = router;
