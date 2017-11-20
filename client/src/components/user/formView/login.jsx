08cf82a397ab8403e65855d894c308a3c26ec15c
Rebecca Townsend <rebecca.townsend722@gmail.com>
2017-11-20 15:54:35 -0800

    Set up login form

--- client/src/components/user/formView/login.jsx
+++ client/src/components/user/formView/login.jsx
@@ -4,15 +4,38 @@ import ReactDOM from 'react-dom';
 class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  onUsernameChange(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onPasswordChange(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  onSubmit() {
+    console.log('clicked submit');
+    this.props.logInUser(this.state.username, this.state.password);
   }
 
   render() {
     return (
       <div>
         <div>Login Form: This will be rendered conditionally</div>
-        <label className="login-username">Username: <input type="text" placeholder="username..."></input></label>
+        <label className="login-username">Username: <input type="text" placeholder="username..." onChange={this.onUsernameChange.bind(this)}></input></label>
         <br></br>
-        <label className="login-password">Password: <input type="password" placeholder="password..."></input></label>
+        <label className="login-password">Password: <input type="password" placeholder="password..." onChange={this.onPasswordChange.bind(this)}></input></label>
+        <br></br>
+        <button className="login-button" onClick={this.onSubmit.bind(this)}>Log In</button>
       </div>
     );
   }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 import Game from './components/user/tictactoeView/game.jsx';
 import Login from './components/user/formView/login.jsx';
 import Signup from './components/user/formView/signup.jsx';
@@ -12,15 +13,51 @@ class App extends React.Component {
     }
   }
 
+  //MAKE SURE THIS INTERACTS CORRECTLY WITH SERVER/DB
+  //On successful user creation, should redirect to login page
+  //On failed user creation (ie: user already in use), should redirect to create user page with descriptive error message
   createUser(username, password, admin) {
-    console.log(username, password, admin);
+    console.log(` ${username}, ${password}, ${admin} posted to server`);
+    $.ajax({
+      method: 'POST',
+      url: '/signup',
+      data: {
+        username: username,
+        password: password,
+        admin: admin
+      },
+      success: (data) => {
+        console.log(success);
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
+
+  logInUser(username, password) {
+    console.log(`${username}, ${password} posted to server`);
+    $.ajax({
+      method: 'POST',
+      url: '/login',
+      data: {
+        username: username,
+        password: password
+      },
+      success: (data) => {
+        console.log(data);
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
   }
 
   render() {
     return (
       <div>
       <Game/>
-      <Login/>
+      <Login logInUser={this.logInUser.bind(this)}/>
       <Signup createUser={this.createUser.bind(this)}/>
       </div>
     )
