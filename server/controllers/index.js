e6ebb303d4a5dc58f74e8c91c10436e47fd540c5
amboz <abozigian@gmail.com>
2017-11-20 17:01:20 -0800

    commenting out controller strategy

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -11,6 +11,8 @@ module.exports = {
     //creates a new user or finds an already existing user
     post: (req, res) => {
       db.User.findOrCreate({
+        //***TODO*** add PW to created user, handle incorrect PW. takes admin and pw
+          //***TODO***: check is username exists, if yes find. if no, create with given PW
         where: {username: req.body.username}
       })
       .spread((user, created) => {
@@ -22,18 +24,17 @@ module.exports = {
 
   //login controller
   login: {
-    get: (req, res) => {
-      // display user info from submissions table
-      console.log('login get');
-    },
+    //***TODO***: authenticate user (check username and matching pw, hashing?)
     post: (req, res) => {
-      // authenticate user
       console.log('login post');
     }
   },
 
   //submissions controller
   submissions: {
+    //***TODO***: display all or some user info on GET from componentDidMount
+      //send username, admin status
+        //if req.body.admin, send everything else send whatever matches username
     get: (req, res) => {
       // if admin
         // display messages from users
@@ -43,10 +44,12 @@ module.exports = {
     },
     post: (req, res) => {
       // user only - add message to submission table
+        //req.body.postUsername
       console.log('submissions post');
     },
     patch: (req, res) => {
       // admin only - add response to existing submission row in sub. table
+        //req.body.postUsername
       console.log('submissions patch');
     }
   }
