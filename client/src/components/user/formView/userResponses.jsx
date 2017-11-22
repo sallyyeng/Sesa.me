83620eaaf9addae28f88d84289a9c35e16e8b22c
Anne Pruett <annepruett@Anne-Pruetts-MacBook.local>
2017-11-22 15:18:12 -0800

    Code cleanup in userResponses component

--- client/src/components/user/formView/userResponses.jsx
+++ client/src/components/user/formView/userResponses.jsx
@@ -15,14 +15,12 @@ class UserResponses extends React.Component {
     this.props.retrieveResponses(this.props.username, (data) => {
       console.log('USER MESSAGES ON SUB COMPONENT', data);
       this.setState({
-        //may have to change 'data' depending on what format the data is returned as
         responses: data
       });
     });
   }
 
 	render(){
-    console.log('hey im the responses in the render function: ', this.state.responses);
 		return (
 			<div className="user-status-main">
 	          <h5>Correspondence history:</h5>
