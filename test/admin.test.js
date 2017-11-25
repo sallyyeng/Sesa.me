7c34d1254de8a209eab8cd826e81bfc39b036891
Rebecca Townsend <rebecca.townsend722@gmail.com>
2017-11-25 14:39:17 -0800

    Create .babelrc. Create framework for testing.

---
+++ test/admin.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AdminView from '../client/src/components/admin/adminView.jsx';
+import Message from '../client/src/components/admin/message.jsx';
+import Response from '../client/src/components/admin/response.jsx';
+import renderer from 'react-test-renderer';
+
+/*TEST ADMIN COMPONENTS*/
+
+//TODO: AdminView
+
+//TODO: MessageView
+
+//TODO: ResponseView
