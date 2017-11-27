affc1e0ed9301224594db1f71b7314d7cf59f927
Anne Pruett <annepruett@Anne-Pruetts-MacBook.local>
2017-11-27 12:03:03 -0800

    Write test for the board

--- test/board.test.js
+++ test/board.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Game from '../client/src/components/user/tictactoeView/game.jsx';
+import Board from '../client/src/components/user/tictacToeView/game.jsx';
 import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 
@@ -8,6 +9,20 @@ import { shallow } from 'enzyme';
 describe('Component: Game', () => {
   it('should match its empty snapshot', () => {
     const component = shallow(<Game/>)
-    expect(component).toMatchSnapShot();
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe('Component: Board', () => {
+  const squares = Array(9).fill(null);
+  var handleClick = function(i){
+    console.log('click handled: ', i);
+  }
+
+  var unlockForms = function(i) {
+  	console.log('forms have been unlocked!');
+  }
+  it('should match its empty snapshot', () => {
+    const component = shallow(<Board squares={squares} onClick={handleClick} unlockForms={unlockForms}/>)
   });
 });
