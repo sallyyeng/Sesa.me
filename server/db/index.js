c1302173f06cfe723744d2c0dd00f5717090cd76
amboz <abozigian@gmail.com>
2017-11-27 20:57:35 -0800

    Working with jeffrey on db configuration

--- server/db/index.js
+++ server/db/index.js
@@ -1,24 +1,34 @@
 // This file initialize a sequelize instance
 // It contains code that defines models, their relationships, and creates the tables IF they don't already exist in mysql
 
-// if (!global.hasOwnProperty('db')) {
-//   var Sequelize = require('sequelize')
-//     , db = null
+try {
+  const config = require('./config.js'); // need to update file path
+} catch (err) {
+  var username = process.env.USER;
+  var port = process.env.DB_PORT;
+  var host = process.env.HOST;
+  var dbUrl = process.env.DATABASE_URL;
+}
 
-//   if (process.env.DATABASE_URL) {
-//     // the application is executed on Heroku ... use the postgres database
-//     db = new Sequelize(process.env.DATABASE_URL, {
-//       dialect:  'postgres',
-//       protocol: 'postgres',
-//       port:     match[4],
-//       host:     match[3],
-//       logging:  true //false
-//     })
-//   } else {
-//     // the application is executed on the local machine ... use mysql
-//     db = new Sequelize('messages', 'root', '', {dialect: 'mysql'});
-//   }
-// }
+
+if (!global.hasOwnProperty('db')) {
+  var Sequelize = require('sequelize')
+    , db = null
+
+  if (process.env.DATABASE_URI) {
+    // the application is executed on Heroku ... use the postgres database
+    db = new Sequelize(, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     port,
+      host:     host,
+      logging:  true //false
+    })
+  } else {
+    // the application is executed on the local machine ... use mysql
+    db = new Sequelize('messages', 'root', '', {dialect: 'mysql'});
+  }
+}
 
 
 
