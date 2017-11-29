00671e0f79f0fb8b70dd46a713e6a0f6207aeb24
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2017-11-29 12:19:17 -0800

    Add Passport.js dependencies

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const passport = require('passport')
+const session = require('express-session')
+const env = require('dotenv').load();
 
 // Middleware
 var parser = require('body-parser');
+
+// For Passport
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // session secret
+
+app.use(passport.initialize());
+
+app.use(passport.session()); // persistent login sessions
 
 // Router
 var router = require('./routes.js');
@@ -11,8 +21,8 @@ var router = require('./routes.js');
 app.set('port', process.env.PORT || 3000);
 
 // Parsing
+app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
-app.use(parser.urlencoded({extended: true}));
 
 // Routes
 app.use('/', router);
