dbe3e57702b895548b438b70dac5f277717a2e1d
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2017-11-29 15:11:25 -0800

    save passport changes before uninstalling vscode

--- server/db/index.js
+++ server/db/index.js
@@ -1,3 +1,75 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require(path.join(__dirname, '../..', 'config', 'config.json'))[env];
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const db = {};
+
+// OG codebase
+// const db = new Sequelize('messages', 'root', '38ankeny', {
+//     dialect: 'mysql'
+// });
+
+fs
+    .readdirSync(__dirname)
+    .filter(function(file) {
+        return (file.indexOf('.') !== 0) && (file !== 'index.js');
+    })
+    .forEach(function(file) {
+        var model = sequelize.import(path.join(__dirname, file));
+        db[model.name] = model;
+    });
+
+Object.keys(db).forEach(function(modelName) {
+    if ('associate' in db[modelName]) {
+        db[modelName].associate(db);
+    }
+});
+
+
+sequelize.sequelize = sequelize;
+sequelize.Sequelize = Sequelize;
+
+module.exports = db;
+
+sequelize.query('CREATE DATABASE IF NOT EXISTS messages').then(() => console.log('Database created'));
+
+
+const User = sequelize.define('user', {
+    //id is already created by default as PK
+    username: { type: Sequelize.STRING, unique: true },
+    hash: Sequelize.STRING,
+    salt: Sequelize.STRING,
+    account_type: Sequelize.STRING,
+    first_name: Sequelize.STRING,
+    last_name: Sequelize.STRING
+});
+
+
+const Submission = sequelize.define('submission', {
+    //id (PK), createdAt, and user id (FK) are created by default
+    user_message: Sequelize.TEXT,
+    user_contact: Sequelize.TEXT,
+    user_urgency: Sequelize.INTEGER,
+    admin_response: Sequelize.TEXT,
+    //Sequelize Boolean will be converted to TINYINT(1)
+    admin_complete: Sequelize.BOOLEAN,
+    first_name: Sequelize.STRING,
+    last_name: Sequelize.STRING
+});
+
+//define 1:many relationship of Users:Submissions
+Submission.belongsTo(User);
+User.hasMany(Submission);
+
+//create tables if they do not yet exist
+User.sync();
+Submission.sync();
+
+exports.User = User;
+exports.Submission = Submission;
+
 // This file initialize a sequelize instance
 // It contains code that defines models, their relationships, and creates the tables IF they don't already exist in mysql
 
@@ -34,16 +106,6 @@
 //   }
 // }
 
-
-
-const Sequelize = require('sequelize');
-// db is named messages
-const db = new Sequelize('messages', 'root', '38ankeny', {
-    dialect: 'mysql'
-});
-
-db.query('CREATE DATABASE IF NOT EXISTS messages').then(() => console.log('Database created'));
-
 // // //DEPLOYMENT DB
 
 // const { Client } = require('pg');
@@ -53,50 +115,14 @@ db.query('CREATE DATABASE IF NOT EXISTS messages').then(() => console.log('Datab
 //   ssl: true,
 // });
 
-// db.connect();
+// sequelize.connect();
 
-// db.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+// sequelize.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
 //   if (err) throw err;
 //   for (let row of res.rows) {
 //     console.log(JSON.stringify(row));
 //   }
-//   // db.end();
+//   // sequelize.end();
 // });
 
 //^^^DEPLOPYMENT DB
-
-
-
-const User = db.define('user', {
-    //id is already created by default as PK
-    username: { type: Sequelize.STRING, unique: true },
-    hash: Sequelize.STRING,
-    salt: Sequelize.STRING,
-    account_type: Sequelize.STRING,
-    first_name: Sequelize.STRING,
-    last_name: Sequelize.STRING
-})
-
-
-const Submission = db.define('submission', {
-    //id (PK), createdAt, and user id (FK) are created by default
-    user_message: Sequelize.TEXT,
-    user_contact: Sequelize.TEXT,
-    user_urgency: Sequelize.INTEGER,
-    admin_response: Sequelize.TEXT,
-    //Sequelize Boolean will be converted to TINYINT(1)
-    admin_complete: Sequelize.BOOLEAN,
-    first_name: Sequelize.STRING,
-    last_name: Sequelize.STRING
-})
-
-//define 1:many relationship of Users:Submissions
-Submission.belongsTo(User);
-User.hasMany(Submission);
-
-//create tables if they do not yet exist
-User.sync();
-Submission.sync();
-
-exports.User = User;
-exports.Submission = Submission;
