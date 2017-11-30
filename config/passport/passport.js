366d0ccd4314404f3d758ab92ba303aab2d37051
sallyyeng@gmail.com <SALLYYENG@GMAIL.COM>
2017-11-30 12:10:32 -0800

    Verify server index.js file and passport.js file align with setup

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -3,91 +3,91 @@ var bCrypt = require('bcrypt-nodejs');
 module.exports = function (passport, user) {
   const User = user;
   const LocalStrategy = require('passport-local').Strategy;
+
+  passport.use('local-signup', new LocalStrategy(
+
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true // allows us to pass back the entire request to the callback
+
+    }, (req, password, done) => {
+      // hash password
+      var generateHash = (password => {
+        return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
+      });
+
+      // store user input data
+      // User.findOne({
+      //   where: {
+      //     email: email
+      //   }
+      // }).then(function (user) {
+      //   if (user) {
+      //     return done(null, false, {
+      //       message: 'That email is already taken'
+      //     });
+      //   } else {
+      var userPassword = generateHash(password);
+      var data =
+        {
+          password: userPassword,
+          firstname: req.body.firstname,
+          lastname: req.body.lastname
+        };
+
+      User.create(data).then(function (newUser, created) {
+
+        if (!newUser) {
+
+          return done(null, false);
+
+        }
+
+        if (newUser) {
+
+          return done(null, newUser);
+
+        }
+      });
+      //     }
+      //   });
+      // }
+    }));
 };
 
-passport.use('local-signup', new LocalStrategy(
+                // module.exports = function (sequelize, Sequelize) {
 
-  {
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true // allows us to pass back the entire request to the callback
+                  //     var User = sequelize.define('user', {
 
-  }, (req, email, password, done) => {
-    // hash password
-    var generateHash = (password => {
-      return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
-    });
+                    //         id: {
+                      //             autoIncrement: true,
+                      //             primaryKey: true,
+                      //             type: Sequelize.INTEGER
+                      //         },
 
-    // store user input data
-    // User.findOne({
-    //   where: {
-    //     email: email
-    //   }
-    // }).then(function (user) {
-    //   if (user) {
-    //     return done(null, false, {
-    //       message: 'That email is already taken'
-    //     });
-    //   } else {
-    var userPassword = generateHash(password);
-    var data =
-      {
-        password: userPassword,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname
-      };
+                      //         firstname: {
+                        //             type: Sequelize.STRING,
+                        //             notEmpty: true
+                        //         },
 
-    User.create(data).then(function (newUser, created) {
+                        //         lastname: {
+                          //             type: Sequelize.STRING,
+                          //             notEmpty: true
+                          //         },
 
-      if (!newUser) {
+                          //         username: {
+                            //             type: Sequelize.TEXT
+                            //         },
 
-        return done(null, false);
+                            //         about: {
+                              //             type: Sequelize.TEXT
+                              //         },
 
-      }
-
-      if (newUser) {
-
-        return done(null, newUser);
-
-      }
-    });
-    //     }
-    //   });
-    // }
-  }));
-
-// module.exports = function (sequelize, Sequelize) {
-
-//     var User = sequelize.define('user', {
-
-//         id: {
-//             autoIncrement: true,
-//             primaryKey: true,
-//             type: Sequelize.INTEGER
-//         },
-
-//         firstname: {
-//             type: Sequelize.STRING,
-//             notEmpty: true
-//         },
-
-//         lastname: {
-//             type: Sequelize.STRING,
-//             notEmpty: true
-//         },
-
-//         username: {
-//             type: Sequelize.TEXT
-//         },
-
-//         about: {
-//             type: Sequelize.TEXT
-//         },
-
-//         email: {
-//             type: Sequelize.STRING,
-//             validate: {
-//                 isEmail: true
+                              //         email: {
+                                //             type: Sequelize.STRING,
+                                //             validate: {
+                                  //                 isEmail: true
 //             }
 //         },
 
--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
-require('dotenv').config();
-
+// require('dotenv').config();
 const express = require('express');
-
 const app = express();
 const passport = require('passport')
 const session = require('express-session')
+const parser = require('body-parser');
 const env = require('dotenv').load();
 const exphbs = require('express-handlebars')
+const router = require('./routes.js');
 
-// blog has models and syncing database in server.js fyi //
+// Body Parser
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
 
-// Middleware
-const parser = require('body-parser');
+// Static Files
+app.use(express.static(`${__dirname}/../client/dist`));
 
 // For Passport
 app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // session secret
@@ -19,30 +21,26 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
 //For Handlebars
-app.set('views', './app/views');
+app.set('views', './server/views');
 app.engine('hbs', exphbs({
   extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
 
-// Router
-const router = require('./routes.js');
+// Import Models
+const models = require('./db/index');
+
+// Auth Routes
+const authRoute = require('./authroute.js')(app);
+
+// load passport strategies
+require('../config/passport/passport.js')(passport, models.User);
+
+// Express Router
+app.use('/', router);
 
 // Set port
 app.set('port', process.env.PORT || 3000);
-
-// Parsing
-app.use(parser.urlencoded({ extended: true }));
-app.use(parser.json());
-
-// Routes
-app.use('/', router);
-
-// Static Files
-app.use(express.static(`${__dirname}/../client/dist`));
-
-// Routes
-var authRoute = require('./authroute.js')(app);
 
 // Init server
 app.listen(app.get('port'));
